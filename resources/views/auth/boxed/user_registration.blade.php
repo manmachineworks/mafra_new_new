2d811afb52226a4aac4a82038b6f5a07d06761b8
Manmachine Works <manmachineworks@gmail.com>
2026-01-06 10:13:03 +0530

    gfdgfg

--- resources/views/auth/boxed/user_registration.blade.php
+++ resources/views/auth/boxed/user_registration.blade.php
@@ -25,7 +25,7 @@
 
                                 <!-- Titles -->
                                 <div class="text-center text-lg-left">
-                                    <h1 class="fs-20 fs-md-24 fw-700 text-primary" style="text-transform: uppercase;">{{ translate('Create an account')}}A</h1>
+                                    <h1 class="fs-20 fs-md-24 fw-700 text-primary" style="text-transform: uppercase;">{{ translate('Create an account')}}cchd</h1>
                                 </div>
 
                                 <!-- Register form -->
